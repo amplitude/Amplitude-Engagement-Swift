f31893d60aa8802a60fff7412b0ffc4d76ed171d
Mike Cavallaro <michaelcavallaro03@gmail.com>
2025-05-21 07:45:49 -0600

    chore: [ios] initial linking of frameworks to swift package

---
+++ Sources/Wrapper/AmplitudeEngagementTarget.swift
@@ -0,0 +1,11 @@
+//
+//  AmplitudeEngagementTarget.swift
+//  Amplitude-Engagement-Swift
+//
+//  Created by Michael Cavallaro on 5/21/25.
+//
+
+// Swift requires a dummy file to be present in the target, even though it's not used
+
+
+ 
